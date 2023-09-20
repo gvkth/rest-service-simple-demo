1eb1da2e39024b1d01149d59dc4a3a16df3a1bf5
gvkth <vokiencuong@gmail.com>
2023-09-20 22:58:57 +0700

    Demo a @PathVariable in api

--- src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -3,7 +3,10 @@ package com.luv2code.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,13 +15,39 @@ import com.luv2code.springdemo.entity.Student;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+	
+	static List<Student> theStudents;
+	
+	//define @PostConstruct to load the student data ... only once
+	@PostConstruct
+	public void loadData() {
+		theStudents = new ArrayList<>(); 
+		theStudents.add(new Student("A1","B1")); 
+		theStudents.add(new Student("A2","B2"));
+		theStudents.add(new Student("A3","B3"));
+				 
+	}
+	
+	/*
+	 * static { theStudents = new ArrayList<>(); theStudents.add(new
+	 * Student("A1","B1")); theStudents.add(new Student("A2","B2"));
+	 * theStudents.add(new Student("A3","B3"));
+	 * System.out.println("===================================abc"); }
+	 */
+	
 	//define endpoint for "/students" - return list of students
 	@GetMapping("/students")
 	public List<Student> getStudents(){
-		List<Student> theStudents = new ArrayList<>();
-		theStudents.add(new Student("A1","B1"));
-		theStudents.add(new Student("A2","B2"));
-		theStudents.add(new Student("A3","B3"));
+		
 		return theStudents;
+	}
+	
+	
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+		Student tempStudent = new Student("Unknown number: "+Integer.toString(studentId),"UFO");
+		if(studentId<theStudents.size() && studentId>=0)
+			tempStudent = theStudents.get(studentId);
+		return tempStudent;
 	}
 }
